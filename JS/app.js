8dda932aa13f4a92a35aeabc73dcff582246de1d
Mrrobotcode <jenuelmercedes27@gmail.com>
2024-04-05 01:01:33 -0400

    Esto son los archivos

---
+++ JS/app.js
@@ -0,0 +1,27 @@
+let left = document.getElementById('left');
+let right = document.getElementById('right');
+var leftNum = '01'; 
+var rightNum ='02';
+
+left.addEventListener('click', function () {
+    rightNum = getImage();
+    right.setAttribute('src', `/img/${rightNum}.png`);
+});
+
+right.addEventListener('click', function () {
+    leftNum = getImage();
+    left.setAttribute('src', `/Img/${getImage()}.png`);
+});
+
+function getImage() {
+    let zero = '0';
+    let number = Math.ceil(Math.random() * 10);
+    if (number != 10) {
+        number = zero + number;
+}
+
+if (number != leftNum && number != rightNum) {
+    return number;}
+else {
+    return getImage();
+}}
